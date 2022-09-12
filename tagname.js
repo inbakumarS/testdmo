22f1c471451d2f69c651e56b1b5a1aec13540675
inbakumarS <inbakumar1231>
2022-09-12 21:22:12 +0530

    new tagjs added

---
+++ tagname.js
@@ -0,0 +1,12 @@
+let li=document.getElementsByTagName('li');
+console.log(li);
+console.log(li[1]);
+li[i].textContent='hello';
+li[2].style.backgroundC0lor='#f4f4f4';
+
+
+//const list=document.querySelectorAll('li');
+for(let i=0;i<li.length;i++){
+    li[i].style.color='red';
+}
+//console.log(list);
