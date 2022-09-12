9241011906fb8846e346143550a31b91f442b1f7
inbakumarS <inbakumar1231>
2022-09-12 21:10:30 +0530

    added commit

---
+++ array.js
@@ -0,0 +1,11 @@
+const fruits=['apple','orange','lemon'];
+
+fruits[3]='grapes';
+
+
+fruits.push('mangos');
+fruits.unshift('berries');
+fruits.pop();
+console.log(Array.isArray('hello'));
+console.log(fruits.indexOf('orange'))
+console.log(fruits);
