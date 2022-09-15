ef32a381d2561d8db36454f40fb397445b9bb2fd
inbakumar <113346537+inbakumarS@users.noreply.github.com>
2022-09-15 23:28:44 +0530

    commit new

---
+++ userpage.js
@@ -0,0 +1,48 @@
+
+const myForm = document.querySelector('#my-form');
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#email');
+const msg = document.querySelector('.msg');
+const userList = document.querySelector('#users');
+
+
+myForm.addEventListener('submit', onSubmit);
+
+function onSubmit(e) {
+  e.preventDefault();
+  
+  if(nameInput.value === '' || emailInput.value === '') {
+    // alert('Please enter all fields');
+    msg.classList.add('error');
+    msg.innerHTML = 'Please enter all fields';
+
+    // Remove error after 3 seconds
+    setTimeout(() => msg.remove(), 3000);
+  } else {
+
+    const li = document.createElement('li');
+
+    li.appendChild(document.createTextNode(`${nameInput.value}: ${emailInput.value}`));
+
+    userList.appendChild(li);
+
+    nameInput.value = '';
+    emailInput.value = '';
+  }
+}
+
+function editUserDetails(name,email){
+    document.getElementById('name').value=name;
+    document.getElementById('email').value=email;
+    
+}
+
+function saveUserDetails(event){
+    event.preventDefault();
+    const name=event.target.name.value;
+    const email=event.target.email.value;
+    localStorage.setItem('name',name);
+    localStorage.setItem('email',email);
+
+}
+localStorage.setItem('name','email', JSON.stringify(saveUserDetails));
